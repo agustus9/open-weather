ac12992d693a03450d5a46b8cf3d840bfe5d6759
Mack <amack@proforma.com>
2018-03-22 01:45:39 -0400

    added element button

--- public/main.js
+++ public/main.js
@@ -1,5 +1,45 @@
+"http://samples.openweathermap.org/data/2.5/forecast?q=London,uk&appid=2b15c90140172d77aa2145870128752c"
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  const site = 'http://api.openweathermap.org/data/2.5/'
+  const weatherKEY = "2b15c90140172d77aa2145870128752c";
+
+  const btn = document.createElement("Button");
+  const t = document.createTextNode("Click Me");
+  btn.appendChild(t);
+  document.body.appendChild(btn);
+
+  
+  btn.addEventListener("click", (event) => {
+    console.log("clicked");
+    const fetchMe = `${site}forecast?q=London,uk&appid=${weatherKEY}`;
+    console.log(fetchMe);
+    fetch(fetchMe).then((response) => {
+      console.log(response.status);
+      if (response.status === 200) {
+        return response.json()
+      }
+
+  // const current = document.querySelector(".click");
+  // current.addEventListener("click", (event) => {
+  //   console.log("clicked");
+  //   const fetchMe = `${site}forecast?q=London,uk&appid=${weatherKEY}`;
+  //   console.log(fetchMe);
+  //   fetch(fetchMe).then((response) => {
+  //     console.log(response.status);
+  //     if (response.status === 200) {
+  //       return response.json()
+  //     }
+    }).then((weather) => {
+      console.log(weather);
+      const output = document.querySelector(".data-log")
+      output.textContent = `${weather.name}`;
+    });
+  })
+
+  console.log("display city");
+
+  document.querySelector('h1').textContent += ''
 }
 
 document.addEventListener('DOMContentLoaded', main)
